72d8f6cef2bee24586a280e8ace25bdab8c33b26
gitccl <chenchuanle6@gmail.com>
2020-10-24 15:14:38 +0800

    build(admin): 添加管理员实体; dao接口和xml文件;

---
+++ src/main/java/cn/akwing/sms/mapper/AdminMapper.java
@@ -0,0 +1,16 @@
+package cn.akwing.sms.mapper;
+
+import cn.akwing.sms.pojo.Admin;
+import java.util.List;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    List<Admin> selectAll();
+
+    int updateByPrimaryKey(Admin record);
+}
---
+++ src/main/java/cn/akwing/sms/pojo/Admin.java
@@ -0,0 +1,73 @@
+package cn.akwing.sms.pojo;
+
+public class Admin {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String gender;
+
+    private String email;
+
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone == null ? null : telephone.trim();
+    }
+}

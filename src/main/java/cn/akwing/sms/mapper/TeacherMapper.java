ac8393ad5cf529b4e06bdc07dd318fca1aa23ba0
gitccl <chenchuanle6@gmail.com>
2020-10-27 16:36:34 +0800

    build(course & teacher): 添加pojo&mapper

---
+++ src/main/java/cn/akwing/sms/mapper/TeacherMapper.java
@@ -0,0 +1,16 @@
+package cn.akwing.sms.mapper;
+
+import cn.akwing.sms.pojo.Teacher;
+import java.util.List;
+
+public interface TeacherMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Teacher record);
+
+    Teacher selectByPrimaryKey(String id);
+
+    List<Teacher> selectAll();
+
+    int updateByPrimaryKey(Teacher record);
+}
---
+++ src/main/java/cn/akwing/sms/pojo/Teacher.java
@@ -0,0 +1,63 @@
+package cn.akwing.sms.pojo;
+
+public class Teacher {
+    private String id;
+
+    private String password;
+
+    private String name;
+
+    private String sex;
+
+    private String tel;
+
+    private String dept;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel == null ? null : tel.trim();
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept == null ? null : dept.trim();
+    }
+}
